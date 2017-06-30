87cc83bf3fec0c4666ed72683f3d08f7d2d3d1ea
yangz1 <yeongtze@googlemail.com>
2017-06-30 05:54:23 -0400

    Created NUnit test project.

---
+++ Synapse.Dns.Tests/DnsTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Synapse.Dns.Tests
+{
+    [TestFixture]
+    public class DnsTests
+    {
+        [Test]
+        public void CreateARecord_With_Valid_Details_Succeed()
+        {
+        }
+    }
+}
---
+++ Synapse.Dns.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "Synapse.Dns.Tests" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Microsoft Corporation" )]
+[assembly: AssemblyProduct( "Synapse.Dns.Tests" )]
+[assembly: AssemblyCopyright( "Copyright © Microsoft Corporation 2017" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+[assembly: ComVisible( false )]
+
+[assembly: Guid( "98d30d2b-73d9-4829-b388-7b07421151df" )]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
