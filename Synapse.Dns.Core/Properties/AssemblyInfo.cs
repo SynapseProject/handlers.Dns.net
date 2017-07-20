977dc8c823e04cc7842270e45d08849e076e7682
yangz1 <yeongtze@googlemail.com>
2017-07-20 05:11:28 -0400

    Renamed output assembly for Core.

--- Synapse.Dns.Core/Properties/AssemblyInfo.cs
+++ Synapse.Dns.Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "handlers.Dns.net" )]
+[assembly: AssemblyTitle( "Synapse.Dns.Core" )]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "handlers.Dns.net" )]
+[assembly: AssemblyProduct( "Synapse.Dns.Core" )]
 [assembly: AssemblyCopyright( "Copyright © Microsoft Corporation 2017" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
